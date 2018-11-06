0c433f62f788b256ac9698ddb12a2cd51f0463c7
Nycholas de Sousa <nycholas@live.com>
2018-11-06 09:06:59 -0300

    Dummy base com imagens

--- src/main/java/com/ufpb/config/LoadDatabase.java
+++ src/main/java/com/ufpb/config/LoadDatabase.java
@@ -23,10 +23,10 @@ class LoadDatabase {
             Random gerador = new Random();
 
             People[] peoples1 = new People[5];
-            peoples1[0] = new People("caio", "caio", "Caio", "Marcelo", "caiomcg@gmail.com", "83911111111", "");
-            peoples1[1] = new People("nycholas", "nycholas", "Nycholas", "de Sousa", "nycholas@live.com", "83922222222", "");
-            peoples1[2] = new People("thiago", "thiago", "Thiago", "Luiz", "thiago.luiz@lavid.ufpb.br", "83933333333", "");
-            peoples1[3] = new People("victor", "victor", "Victor", "Batista", "rotciv18@hotmail.com", "83944444444", "");
+            peoples1[0] = new People("caio", "caio", "Caio", "Marcelo", "caiomcg@gmail.com", "83911111111", "https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/15941174_1610327608981623_1952268802427833851_n.jpg?_nc_cat=108&_nc_ht=scontent.frec10-1.fna&oh=b754df73b6008cc9c2de16f9ac870481&oe=5C82CDF9");
+            peoples1[1] = new People("nycholas", "nycholas", "Nycholas", "de Sousa", "nycholas@live.com", "83922222222", "https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/11813379_1659635650940746_1378281156469120823_n.jpg?_nc_cat=101&_nc_ht=scontent.frec10-1.fna&oh=1c12c3ee2eb229c2fbd946e1302a8e2f&oe=5C8AACE6");
+            peoples1[2] = new People("thiago", "thiago", "Thiago", "Luiz", "thiago.luiz@lavid.ufpb.br", "83933333333", "https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-9/11026214_1023510014347002_8947323924973015120_n.jpg?_nc_cat=106&_nc_ht=scontent.frec10-1.fna&oh=f9cfb9fa9421749d09cdd6b4d486e858&oe=5C42329D");
+            peoples1[3] = new People("victor", "victor", "Victor", "Batista", "rotciv18@hotmail.com", "83944444444", "https://scontent.frec10-1.fna.fbcdn.net/v/t1.0-1/30652385_2106410726043429_494501650803453206_n.jpg?_nc_cat=107&_nc_ht=scontent.frec10-1.fna&oh=c879beff100439cc78302d6528acd479&oe=5C408575");
             peoples1[4] = new People("gledson", "gledson", "Gledson", "Elias", "gledson@ci.ufpb.br", "83955555555", "http://www.ufpb.br/sites/default/files/Gledson%20Silveira.jpg");
 
             for (int i = 0; i < peoples1.length; i++) {
